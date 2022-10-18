970035601922f3e87653a3c002b4b4e46d44eadf
brokkko <nsstrannikova@mail.ru>
2022-10-18 08:01:01 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from "path";
+const app = express();
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'views')))
+app.set('view-engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index.pug', { pageTitle: "Nikita Loh", youAreUsingPug: true })
+})
+
+const host = '127.0.0.1';
+const port = 7000;
+app.listen(port, host, function () {
+    console.log(`Server listens http://${host}:${port}`)
+})
